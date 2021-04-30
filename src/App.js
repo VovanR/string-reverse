5d332fa088ffb7a502ee8335ccb6db9a3bed80a1
Vladimir Rodkin <mail@vovanr.com>
2021-04-30 18:02:08 +0300

    Add base functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import {useState, useCallback} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState('')
+
+  const handleChange = useCallback(({target: {value}}) => {
+    setValue(value)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        value={value}
+        onChange={handleChange}
+      />
+
+      <textarea
+        value={value.split('').reverse('').join('')}
+        readOnly={true}
+      />
     </div>
   );
 }
